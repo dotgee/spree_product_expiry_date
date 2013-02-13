c0929f6f52767bb334ea66a0433f9b2afc3f6eaf
Johann <johann@dotgee.fr>
2013-02-13 18:26:55 +0100

    Test (I know...)

--- app/overrides/hide_products_on_expiry_date.rb
+++ app/overrides/hide_products_on_expiry_date.rb
@@ -1,7 +1,14 @@
 Deface::Override.new(
   :virtual_path       => "spree/shared/_products",
   :name               => "hide_products_on_expiry_date",
-  :replace_contents   => '#products',
-  :partial            => "spree/shared/products_modified",
+  :replace            => "#products > code[erb-silent]:contains('if ')",
+  :text               => "if product.on_display? && !(product.preferred_hide_on_expire && Time.now > product.expiry_date)",
   :disabled           => false
 )
+# Deface::Override.new(
+#   :virtual_path       => "spree/shared/_products",
+#   :name               => "hide_products_on_expiry_date",
+#   :replace_contents   => '#products',
+#   :partial            => "spree/shared/products_modified",
+#   :disabled           => false
+# )
