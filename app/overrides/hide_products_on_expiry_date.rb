d7ff378773ab39a4fb54299e2970c5c22c33cac1
Johann <johann@dotgee.fr>
2013-02-20 09:18:55 +0100

    Compatibility for bicf mag

--- app/overrides/hide_products_on_expiry_date.rb
+++ app/overrides/hide_products_on_expiry_date.rb
@@ -1,7 +1,8 @@
 Deface::Override.new(
   :virtual_path       => "spree/shared/_products",
   :name               => "hide_products_on_expiry_date",
-  :replace_contents   => '#products',
+  :replace_contents   => "code[erb-silent]:contains('if products.any?')",
+  :closing_selector   => "code[erb-silent]:contains('end')",
   :partial            => "spree/shared/products_modified",
   :disabled           => false
 )
