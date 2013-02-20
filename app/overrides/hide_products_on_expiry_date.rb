231a587d5716458ef53aa4b795d143a8df6a160a
Johann <johann@dotgee.fr>
2013-02-20 09:46:42 +0100

    Fix

--- app/overrides/hide_products_on_expiry_date.rb
+++ app/overrides/hide_products_on_expiry_date.rb
@@ -1,8 +1,8 @@
 Deface::Override.new(
   :virtual_path       => "spree/shared/_products",
   :name               => "hide_products_on_expiry_date",
-  :replace   => "code[erb-silent]:contains('if products.any?')",
-  :closing_selector   => "code[erb-silent]:contains('end')",
+  :replace            => "code[erb-silent]:contains('if products.any?')",
+  :closing_selector   => "code[erb-silent]:contains('#end for override')",
   :partial            => "spree/shared/products_modified",
   :disabled           => false
 )
